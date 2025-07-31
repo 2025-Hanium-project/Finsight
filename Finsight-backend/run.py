b8884479345580013f5180da2b0124c4863207ce
etluser <etluser@node3.finsight.com>
2025-07-31 23:32:12 +0900

    FIXED : flask server port changed to 5500

--- Finsight-backend/run.py
+++ Finsight-backend/run.py
@@ -6,4 +6,4 @@ env = os.getenv("FLASK_ENV", "dev")    # dev, test, prod 중 하나
 app = create_app(config_name=env)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=(env=="dev"), use_reloader=True)
+    app.run(host="0.0.0.0", port=5500, debug=(env=="dev"), use_reloader=True)
